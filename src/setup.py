8a4015d1fdc25c49593914fcb15c96026af835a2
Adrian Mak <damarkian@gmail.com>
2020-07-14 05:56:25 +0800

    boilerplate

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name                            = "schmoopie",
+    packages                        = setuptools.find_packages(where = "python"),
+    package_dir                     = {
+        "" : "python"
+    },
+    classifiers                     = (
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
+

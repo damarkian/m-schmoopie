97059fc3987ed625afb36a1a7026a6e311fbf055
Adrian Mak <damarkian@gmail.com>
2020-07-14 18:26:56 +0800

    a real good time...

--- src/python/schmoopie/qmolecule.py
+++ src/python/schmoopie/qmolecule.py
@@ -3,26 +3,11 @@ import h5py
 from openfermion.hamiltonians import MolecularData
 from openfermionpsi4 import run_psi4
 
-def make_geometry():
-    # boilerplate
 
-    geometry = [
-                ['H', [0.0, 0.0, 0.0]], 
-                ['H', [0.0, 0.0, 0.8]]
-               ]
 
-    return geometry
+def create_molecule(geometry, basisset, multiplicity, charge, description, moleculefilename):
 
-def create_molecule():
-
-    geometry = make_geometry()
-    basis = 'sto-3g'
-    charge = 0
-    multiplicity = 1
-    moleculefilename = '/app/moleculefile.hdf5'
-
-    molecule = MolecularData(geometry, basis, multiplicity, charge, description, filename=moleculefilename)
-    
+    molecule = MolecularData(geometry, basis, multiplicity, charge, description, filename=moleculefilename)    
     # openfermion's run_psi4
     molecule = run_psi4(molecule, run_scf=1, run_mp2=1, run_cisd=0, run_ccsd=0, run_fci=0, verbose=1, tolerate_error=1)
     molecule.save()

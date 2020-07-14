efc70bc3975cc697a3ec2ed98747aaca517abc80
Adrian Mak <damarkian@gmail.com>
2020-07-14 18:30:39 +0800

    i feel alive...

--- src/python/schmoopie/qmolecule.py
+++ src/python/schmoopie/qmolecule.py
@@ -7,7 +7,7 @@ from openfermionpsi4 import run_psi4
 
 def create_molecule(geometry, basisset, multiplicity, charge, description, moleculefilename):
 
-    molecule = MolecularData(geometry, basis, multiplicity, charge, description, filename=moleculefilename)    
+    molecule = MolecularData(geometry, basisset, multiplicity, charge, description, filename=moleculefilename)    
     # openfermion's run_psi4
     molecule = run_psi4(molecule, run_scf=1, run_mp2=1, run_cisd=0, run_ccsd=0, run_fci=0, verbose=1, tolerate_error=1)
     molecule.save()
